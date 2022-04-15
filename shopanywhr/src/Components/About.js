02d7559bafb3646db3bf22f0dd6e001be069c8d7
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-04-15 23:06:05 +0530

    contact us

--- shopanywhr/src/Components/About.js
+++ shopanywhr/src/Components/About.js
@@ -10,21 +10,21 @@ const About = () => {
       <Card className="text-center">
         <Card.Header> <h1>  About Us </h1> </Card.Header>
         <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
+              
+              <Card.Text>
+                <h2> Nilesh Arjun Ranpise </h2>
+                <Card.Title>Full-stack Developer </Card.Title>
+              </Card.Text>
+              <Card.Text>
+              <Card.Text> <strong> Mobile No :</strong> +91 7506492989 </Card.Text>
+              <Card.Text> <strong>Email Id : </strong> nilesh.ranpise92@gmail.com </Card.Text>
+              <Card.Text> <strong> Linked In:</strong> https://www.linkedin.com/in/nileshranpise/ </Card.Text>
+              <Card.Text> <strong>Git Hub :</strong> https://github.com/NileshRanpise </Card.Text>
+              <Card.Text> <strong>Location :</strong> Mumbai,Maharashtra </Card.Text>
+              </Card.Text>
+              {/* <Card.Text>Contact for more details</Card.Text> */}
+              <Button variant="success">Contact for more Details</Button>
+            </Card.Body>
         <Card.Footer className="text-muted"> -=|||=-</Card.Footer>
       </Card>
       </Container>
--- shopanywhr/src/Components/Contact.js
+++ shopanywhr/src/Components/Contact.js
@@ -1,55 +1,53 @@
-import React from 'react';
-import { Card, Button, Container,Row,Col } from "react-bootstrap";
+import React from "react";
+import { Card, Button, Container, Row, Col,Form,FloatingLabel } from "react-bootstrap";
+import "../Styles/Card.css"
 
 const Contact = () => {
   return (
     <>
-      <br></br>
-      <h2 style={{align:"centre"}}> Contact Page </h2>
-      <hr></hr>
-      <Container fluid="md">
-      <Row>
-      <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i02.appmifile.com/867_operator_in/23/04/2021/3849c44ac78d65621750a114811711f9.png?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>Mobile</Card.Title>
-            <Card.Text>
-             Amount Rs.25000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
+      <>
+        <Container>
+       
+        <div className="row my-3">
+            <div className="col-sm-6 offset-sm-3">
+                {/* <h3 className="maintitle"> Contact Us </h3> */}
+                <hr></hr>
 
-        <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i01.appmifile.com/webfile/globalimg/in/cms/81DAE22D-023B-1742-5400-32A7760B0962.jpg?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>Laptop</Card.Title>
-            <Card.Text>
-            Amount Rs.35000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i02.appmifile.com/620_operator_in/23/04/2021/3c98ae4c034583122a67669c840f249e.jpg?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>TV</Card.Title>
-            <Card.Text>
-            Amount Rs.30000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        </Row>
-      </Container>
+                <form  className="shadow p-5">
+                <h3 className="maintitle"> Contact Us </h3>
+                  <hr></hr>
+
+                    <div className="mb-3">
+                        <label className="form-label" for="name" placeholder="Enter Name"> Name :</label>
+                        <input type="text" className="form-control" name="name" required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label for="email" className="form-label" placeholder="Enter email" >Email Id :</label>
+                        <input type="email" name="email" className="form-control" required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label for="phone" className="form-label" placeholder="Enter Phone no." >Phone no :</label>
+                        <input type="number" name="phone" className="form-control" required />
+                    </div>
+                    <div >
+                        <button type="submit" id="btn" className="btn btn-danger">Submit</button>
+                    </div>
+                    <br></br>
+                   
+
+                </form>
+               
+            </div>
+        </div>
+    
+
+
+        </Container>
+      </>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
