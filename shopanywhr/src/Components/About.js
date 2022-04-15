fc1af6f3045b9f53549dc6e9ca7c0913380b70f0
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-04-15 21:33:22 +0530

    about us

--- shopanywhr/src/Components/About.js
+++ shopanywhr/src/Components/About.js
@@ -1,55 +1,35 @@
-import React from 'react';
-import { Card, Button, Container,Row,Col } from "react-bootstrap";
+import React from "react";
+import { Card, Button, Container, Row, Col, Jumbotron } from "react-bootstrap";
+import "../Styles/home.css";
 
 const About = () => {
   return (
     <>
-      <br></br>
-      <h2 style={{align:"centre"}}> About Page </h2>
-      <hr></hr>
-      <Container fluid="md">
-      <Row>
-      <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i02.appmifile.com/867_operator_in/23/04/2021/3849c44ac78d65621750a114811711f9.png?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>Mobile</Card.Title>
-            <Card.Text>
-             Amount Rs.25000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
-
-        <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i01.appmifile.com/webfile/globalimg/in/cms/81DAE22D-023B-1742-5400-32A7760B0962.jpg?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>Laptop</Card.Title>
-            <Card.Text>
-            Amount Rs.35000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        <Col md={4}  sm={4}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://i02.appmifile.com/620_operator_in/23/04/2021/3c98ae4c034583122a67669c840f249e.jpg?width=140&height=140" />
-          <Card.Body>
-            <Card.Title>TV</Card.Title>
-            <Card.Text>
-            Amount Rs.30000/-
-            </Card.Text>
-            <Button variant="success">Add to Cart</Button>
-          </Card.Body>
-        </Card>
-        </Col>
-        </Row>
+    <Container>
+    <br></br>
+      <Card className="text-center">
+        <Card.Header> <h1>  About Us </h1> </Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+        <Card.Footer className="text-muted"> -=|||=-</Card.Footer>
+      </Card>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- shopanywhr/src/Components/NavbarComp.js
+++ shopanywhr/src/Components/NavbarComp.js
@@ -4,9 +4,11 @@ import {
   NavDropdown,
   // Form,
   // FormControl,
-  // Button,
+  Button,
   Nav,
   Container,
+  // Dropdown,
+  // Badge,
   // Row,
   // Col
 } from "react-bootstrap";
@@ -17,20 +19,19 @@ import About from "./About";
 import Laptop from "./Products/Laptop";
 import Mobile from "./Products/Mobile";
 import Headphones from "./Products/Headphones";
+import "../Styles/NavbarComp.css";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbarcomp = () => {
   return (
     <>
       <div>
-        <Navbar bg="dark" variant={"dark"} expand="lg">
+        <Navbar bg="dark" variant={"dark"} expand="lg" style={{ height: 80 }}>
           <Container fluid="md">
-            {/* <Container className="m"> */}
-            {/* <Row> */}
-            <Navbar.Brand className="ml-5 ml-lg-0"  active as={Link} to="/home">
+            <Navbar.Brand className="ml-5 ml-lg-0" active as={Link} to="/home">
               ShopAnywhr
             </Navbar.Brand>
-            {/* </Container> */}
-           
+
             <Navbar.Toggle
               className="ml-2 ml-lg-0"
               aria-controls="navbarScroll"
@@ -69,15 +70,7 @@ const Navbarcomp = () => {
                     Headphones
                   </NavDropdown.Item>
                 </NavDropdown>
-                {/* <Nav.Link  active as={Link} to="/mobile">
-                  Mobile
-                </Nav.Link>
-                <Nav.Link  active as={Link} to="/laptop">
-                  Laptop
-                </Nav.Link>
-                <Nav.Link active as={Link} to="/headphones">
-                  Headphones
-                </Nav.Link> */}
+
                 <Nav.Link active as={Link} to="/contact">
                   Contact Us
                 </Nav.Link>
@@ -86,8 +79,11 @@ const Navbarcomp = () => {
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
-            {/* </Row> */}
-            {/* <Container fluid="md"> */}
+            <Nav.Link active as={Link} to="/">
+              <Button variant="success">
+                <FaShoppingCart color="white" fontSize="25px" />{" "}
+              </Button>
+            </Nav.Link>
           </Container>
         </Navbar>
       </div>
